5a4c1b481b2358e236cb2d8e8b2c00a127c5a5e5
Tyler <taib2206011@student.ctu.edu.vn>
2026-06-12 22:45:23 +0700

    preprocess_v2

--- temporal_split_index.py
+++ temporal_split_index.py
@@ -1,6 +1,29 @@
+import pandas as pd
 
-#tạo đường dẫn cho file temporal_split_index
+#ép kiểu phòng trường hợp pandas đọc thành int và có số 0 đầu sẽ bỏ số 0 đầu sẽ làm sai dữ liệu khi tạo node
+dtype={"From Bank":str, "To Bank": str, "Account":str, "Account.1": str}
+df_trans=pd.read_csv("dataset_high/HI-Small_Trans.csv")
 
-#ép kiểu string để không bị sai vì account name và bank ID là kiểu ký tự số định danh khác nhau
+#sắp xếp dataframe theo timestamp
+order = df_trans["Timestamp"].sort_values(kind="mergesort").index
+df_trans=df_trans.iloc[order].reset_index(drop=True)
 
-#
+#chia dữ liệu theo train/test/val
+n=len(df)
+t1=int(n*0.6)
+t2=int(n*0.8)
+ts1=df_trans["Timestamp"].iloc[t1]
+ts2=df_trans["Timestamp"].iloc[t2]
+
+# gán nhãn train/test/value cho tập dữ liệu mới và xuất file
+df_trans["split"]="test"
+df_trans.loc[df_trans["Timestamp"]<ts2,"split"]="val"
+df_trans.loc[df_trans["Timestamp"]<ts1,"split"]="train"
+assert df[df.split == "train"]["Timestamp"].max() < df[df.split == "val"]["Timestamp"].min(), \
+    "Leakage: Timestamp chong lan giua train va val"
+assert df[df.split == "val"]["Timestamp"].max() < df[df.split == "test"]["Timestamp"].min(), \
+    "Leakage: Timestamp chong lan giua val va test"
+
+# xuất file csv
+df_trans.to_csv("dataset_high/HI-Small_Trans_split_index.csv", index=False)
+print(f"Đã lưu: {dataset_high/HI-Small_Trans_split_index.csv}")
